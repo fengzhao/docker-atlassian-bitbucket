1a703aaf735f0173d02572f56ab88afbf4e095aa
fengzhao <fengzhao1124@hotmail.com>
2021-04-01 20:38:00 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,97 @@
 #!/bin/bash
+#
+# A helper script for ENTRYPOINT.
+#
+# If first CMD argument is 'bitbucket', then the script will start bitbucket
+# If CMD argument is overriden and not 'bitbucket', then the user wants to run
+# his own process.
 
-# check if the `server.xml` file has been changed since the creation of this
-# Docker image. If the file has been changed the entrypoint script will not
-# perform modifications to the configuration file.
-if [ "$(stat --format "%Y" "${BITBUCKET_INSTALL}/conf/server.xml")" -eq "0" ]; then
-  if [ -n "${X_PROXY_NAME}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_PORT}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_SCHEME}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ "${X_PROXY_SCHEME}" = "https" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="7990"]' --type "attr" --name "secure" --value "true" "${BITBUCKET_INSTALL}/conf/server.xml"
-    xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="7990"]/@redirectPort' --value "${X_PROXY_PORT}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PATH}" ]; then
-    xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${BITBUCKET_INSTALL}/conf/server.xml"
-  fi
+set -e
+
+[[ ${DEBUG} == true ]] && set -x
+
+bitbucket_embedded_search="false"
+
+if [ -n "${BITBUCKET_EMBEDDED_SEARCH}" ]; then
+  bitbucket_embedded_search=${BITBUCKET_EMBEDDED_SEARCH}
 fi
 
-exec "$@"
+function updateBitbucketProperties() {
+  local propertyfile=$1
+  local propertyname=$2
+  local propertyvalue=$3
+  set +e
+  grep -q "${propertyname}=" ${propertyfile}
+  if [ $? -eq 0 ]; then
+    set -e
+    if [[ $propertyvalue == /* ]]; then
+      sed -i "s/\(${propertyname/./\\.}=\).*\$/\1\\${propertyvalue}/" ${propertyfile}
+    else
+      sed -i "s/\(${propertyname/./\\.}=\).*\$/\1${propertyvalue}/" ${propertyfile}
+    fi
+  else
+    set -e
+    echo "${propertyname}=${propertyvalue}" >> ${propertyfile}
+  fi
+}
+
+function processBitbucketProxySettings() {
+  if [ -n "${BITBUCKET_CONTEXT_PATH}" ] || [ -n "${BITBUCKET_PROXY_NAME}" ] || [ -n "${BITBUCKET_PROXY_PORT}" ] || [ -n "${BITBUCKET_DELAYED_START}" ] || [ -n "${BITBUCKET_CROWD_SSO}" ] ; then
+    if [ ! -f ${BITBUCKET_HOME}/bitbucket.properties ]; then
+      touch ${BITBUCKET_HOME}/bitbucket.properties
+    fi
+  fi
+
+  if [ -n "${BITBUCKET_CONTEXT_PATH}" ]; then
+    updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.context-path" ${BITBUCKET_CONTEXT_PATH}
+  fi
+
+  if [ -n "${BITBUCKET_PROXY_NAME}" ]; then
+    updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.proxy-name" ${BITBUCKET_PROXY_NAME}
+  fi
+
+  if [ -n "${BITBUCKET_PROXY_PORT}" ]; then
+    updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.proxy-port" ${BITBUCKET_PROXY_PORT}
+  fi
+
+  if [ -n "${BITBUCKET_PROXY_SCHEME}" ]; then
+    if [ "${BITBUCKET_PROXY_SCHEME}" = 'https' ]; then
+      local secure="true"
+      updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.secure" ${secure}
+      updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.scheme" ${BITBUCKET_PROXY_SCHEME}
+    else
+      local secure="false"
+      updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.secure" ${secure}
+      updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "server.scheme" ${BITBUCKET_PROXY_SCHEME}
+    fi
+  fi
+
+  if [ -n "${BITBUCKET_CROWD_SSO}" ] ; then
+    updateBitbucketProperties ${BITBUCKET_HOME}/bitbucket.properties "plugin.auth-crowd.sso.enabled" ${BITBUCKET_CROWD_SSO}
+  fi
+}
+
+if [ -n "${BITBUCKET_DELAYED_START}" ]; then
+  sleep ${BITBUCKET_DELAYED_START}
+fi
+
+processBitbucketProxySettings
+
+# If there is a 'ssh' directory, copy it to /home/bitbucket/.ssh
+if [ -d /var/atlassian/bitbucket/ssh ]; then
+  mkdir -p /home/bitbucket/.ssh
+  cp -R /var/atlassian/bitbucket/ssh/* /home/bitbucket/.ssh
+  chmod -R 700 /home/bitbucket/.ssh
+fi
+
+if [ "$1" = 'bitbucket' ] || [ "${1:0:1}" = '-' ]; then
+  #source /usr/bin/dockerwait
+  umask 0027
+  if [ "${bitbucket_embedded_search}" = 'true' ]; then
+    exec ${BITBUCKET_INSTALL}/bin/start-bitbucket.sh -fg
+  else
+    exec ${BITBUCKET_INSTALL}/bin/start-bitbucket.sh --no-search -fg
+  fi
+else
+  exec "$@"
+fi
